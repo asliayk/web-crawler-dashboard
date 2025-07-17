dbba4af405722ecfd9c21cfbb5db6fb47d0dc4bb
Asli Aykan <asliaykan97@gmail.com>
2025-07-17 05:16:57 +0200

    Remove .history directory from repository

--- .history/backend/go_20250716221029.mod
+++
@@ -1,9 +0,0 @@
-module crawler
-
-go 1.21
-
-require (
-    github.com/gin-gonic/gin v1.10.0
-    github.com/go-sql-driver/mysql v1.6.0
-    github.com/PuerkitoBio/goquery v1.8.0
-)
--- .history/backend/models/broken_link_20250717031058.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type BrokenLink struct {
-	ID     int    `json:"id"`
-	URLID  int    `json:"url_id"`
-	Link   string `json:"link"`
-	Status int    `json:"status"`
-} 
